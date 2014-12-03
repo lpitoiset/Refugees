99cfb10e0c10585b5324c1b8c461984f47855549
Laurent Pitoiset <github@pitoiset.net>
2014-12-03 22:03:15 +0100

    navigation refugees info + popstats

---
+++ app/src/main/java/com/example/refugeesinfo/CountryList.java
@@ -0,0 +1,21 @@
+package com.example.refugeesinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class CountryList extends Activity {
+
+    protected String[] countries = {"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Argentina"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_pop_stats);
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        ListView countryList = (ListView) findViewById(R.id.CountryList);
+        countryList.setAdapter(adapter);
+    }
+}
